6249d4f54a79baf3f0c9d32e9d751581bba0b0ae
Federica Barrera <fede@MacBook-Air-de-Feder.local>
2026-05-05 11:57:26 -0500

    first commit

---
+++ punteros_v1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num=15, a;
+    int *x;
+
+    x = &num; //a x se asigna la direccion de num
+    a = *x; // a se le asigna el valor de x
+    printf("%p %d\n",x,a);
+
+    char letra= 'a', c;
+    char *ptr;
+    ptr = &letra;
+    c = *ptr;
+    printf("%p %c", ptr, c);
+
+    float decimal= 3.14, dec;
+    float *ptrF;
+    ptrF = &decimal;
+    dec = *ptrF;
+    printf("%p %2.2f" , ptrF, dec);
+
+    return 0;
+}
