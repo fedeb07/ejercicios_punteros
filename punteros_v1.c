1a230374b7a72acf01ac0d36c123bee92ecd5721
Federica Barrera <fede@MacBook-Air-de-Feder.local>
2026-05-05 12:14:04 -0500

    Actualizo código de punteros con mejoras

--- punteros_v1.c
+++ punteros_v1.c
@@ -1,4 +1,7 @@
+// Programa de ejemplo de punteros en C
 #include <stdio.h>
+
+// Uso de punteros con enteros
 int main(int argc, char const *argv[])
 {
     int num=15, a;
@@ -7,13 +10,15 @@ int main(int argc, char const *argv[])
     x = &num; //a x se asigna la direccion de num
     a = *x; // a se le asigna el valor de x
     printf("%p %d\n",x,a);
-
+    
+    // Uso de punteros con caracteres
     char letra= 'a', c;
     char *ptr;
     ptr = &letra;
     c = *ptr;
     printf("%p %c", ptr, c);
 
+    // Uso de punteros con flotantes
     float decimal= 3.14, dec;
     float *ptrF;
     ptrF = &decimal;
@@ -22,3 +27,4 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
+//Fin del programa 
